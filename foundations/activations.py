f3f873d374504dab4e2fcea42011616f1c683995
trijala <trijala97@gmail.com>
2026-07-02 13:44:45 -0500

    Implement Activation functions (Math Foundations 2/6)

---
+++ foundations/activations.py
@@ -0,0 +1,10 @@
+class Solution:
+    def get_minimizer(self, iterations: int, learning_rate: float, init: int) -> float:
+        # Objective function: f(x) = x^2
+        # Derivative:         f'(x) = 2x
+        # Update rule:        x = x - learning_rate * f'(x)
+        # Round final answer to 5 decimal places
+        temp = init
+        for i in range(iterations):
+            temp = temp - (learning_rate * 2 * temp)
+        return round(temp,5)
